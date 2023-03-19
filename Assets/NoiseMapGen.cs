e982f530d23d8a4a415b4df3a461ee895919e06f
Mario Egocheaga <43833659+Mario-Egocheaga@users.noreply.github.com>
2023-03-18 21:31:26 -0400

    Terrain

--- Assets/NoiseMapGen.cs
+++ Assets/NoiseMapGen.cs
@@ -5,37 +5,44 @@ using UnityEngine;
 public class NoiseMapGen : MonoBehaviour
 {
     [System.Serializable]
-
     public class Wave
     {
         public float seed;
         public float frequency;
         public float amplitude;
-
-        public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale, float offsetX, float offsetZ, Wave[] waves)
-        {
-            float[,] noiseMap = new float[mapDepth, mapWidth];
-
-            for (int zIndex = 0; zIndex < mapDepth; zIndex++)
-            {
-                for (int xIndex = 0; xIndex < mapWidth; xIndex++)
-                {
-                    float sampleX = (xIndex + offsetX) / scale;
-                    float sampleZ = (zIndex + offsetZ) / scale;
-
-                    float noise = 0f;
-                    float normalization = 0f;
-                    foreach (Wave wave in waves)
-                    {
-                        noise += wave.amplitude * Mathf.PerlinNoise(sampleX * wave.frequency + wave.seed, sampleZ * wave.frequency + wave.seed);
-                        normalization += wave.amplitude;
-                    }
-                    noise /= normalization;
-                    noiseMap[zIndex, xIndex] = noise;
-                }
-            }
-
-            return noiseMap;
-        }
     }
+
+    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale, float offsetX, float offsetZ, Wave[] waves)
+    {
+        // create an empty noise map with the mapDepth and mapWidth coordinates
+        float[,] noiseMap = new float[mapDepth, mapWidth];
+
+        for (int zIndex = 0; zIndex < mapDepth; zIndex++)
+        {
+            for (int xIndex = 0; xIndex < mapWidth; xIndex++)
+            {
+                // calculate sample indices based on the coordinates and the scale
+                float sampleX = (xIndex + offsetX) / scale;
+                float sampleZ = (zIndex + offsetZ) / scale;
+
+                // generate noise value using PerlinNoise
+                //float noise = Mathf.PerlinNoise(sampleX, sampleZ);
+                float noise = 0f;
+                float normalization = 0f;
+                foreach (Wave wave in waves)
+                {
+                    // generate noise value using PerlinNoise for a given Wave
+                    noise += wave.amplitude * Mathf.PerlinNoise(sampleX * wave.frequency + wave.seed, sampleZ * wave.frequency + wave.seed);
+                    normalization += wave.amplitude;
+                }
+                // normalize the noise value so that it is within 0 and 1
+                noise /= normalization;
+                noiseMap[zIndex, xIndex] = noise;
+            }
+        }
+
+        return noiseMap;
+    }
+
+
 }
